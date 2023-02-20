c879b681704d49dd3d73af3a6674016f8810ca8c
Xander Renkema <105459386+RedTechGamer236@users.noreply.github.com>
2023-02-19 23:11:25 -0700

    Finished Assignment

--- main.js
+++ main.js
@@ -7,9 +7,13 @@ given in the comments.
 
 // 1) Create an array named myPizzaOrder that cannot be reassigned and
 // contains the 3 elements: "Pepperoni", 12.99, false
+const myPizzaOrder = ["Pepperoni", 12.99, false];
 
 // 2) Create a function named getPizzaType that will accept an array
 // as a parameter and returns the 1st element from the array.
+function getPizzaType(pizzaArray) {
+  return pizzaArray[0];
+}
 
 // 3) Create a function named addTax that will accept an array
 // like your myPizzaOrder array as a parameter. The function will
@@ -19,30 +23,58 @@ given in the comments.
 // The function should return the array.
 // ===========
 // Hint: find the method that will allow you to set the decimals.
+function addTax(purchases) {
+  purchases.splice(2, 0, Math.round(purchases[1] * 0.0925 * 100) / 100);
+  return purchases;
+}
 
 // 4) Create a function named completeOrder that will accept an array
 // as a parameter. The function will change the value of the 4th
 // element in the array to true and return the array.
+function completeOrder(order) {
+  order[3] = true;
+  return order;
+}
 
 // 5) Create a function named allTheNumbers that accepts a number as
 // a parameter. The function will create an array that contains all of
 // the numbers from 1 to whatever number the parameter is. The array
 // should not include the parameter number. The function returns
 // the array.
+function allTheNumbers(maximum) {
+  let allNumbers = [];
+  for (let i = 1; i < maximum; i++) {
+    allNumbers[i - 1] = i;
+  }
+  return allNumbers;
+}
 
 // 6) Create a function named getEvens that accepts 2 parameters:
 // startNum and endNum. The function should return an array that
 // includes the even numbers from startNum to endNum. If the startNum
 // or endNum are even, they should be included in the returned array.
+function getEvens(startNum, endNum) {
+  let evenNumbers = [];
+  for (let i = startNum; i <= endNum; i++) {
+    if (i % 2 === 0) evenNumbers.push(i);
+  }
+  return evenNumbers;
+}
 
 // 7) Create a function named getLastElement that accepts an array
 // as a parameter and returns the last element of the array. The
 // array can have any number of elements. The function should always
 // return the last element.
+function getLastElement(array) {
+  return array[array.length - 1];
+}
 
 // 8) Create a function named combineArrays that accepts two arrays
 // as parameters and combines them into one array. The function
 // should return the new combined array.
+function combineArrays(array1, array2) {
+  return array1.concat(array2);
+}
 
 // 9) Create a function named isPalindrome that accepts a string as
 // a parameter. The function will return true if the string is a
@@ -57,3 +89,12 @@ given in the comments.
 // into an array, use arrays methods, and turn the array back into
 // a string. This is also a challenge on freeCodeCamp. Check the
 // help forums there if you get stuck!
+function isPalindrome(word) {
+  word = word.toLowerCase().replace(/(\W)|(\_)/g, "").split("");
+  for (var i = 0; i < word.length; i++) {
+    if (word[i] !== word[word.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
